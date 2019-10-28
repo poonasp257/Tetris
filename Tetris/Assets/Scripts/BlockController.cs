681c2c7ff0f1fd7efece73523ba2fad0cb3bbbb4
poonasp257 <poonasp257@naver.com>
2019-10-28 16:23:58 +0900

    Update
    # 테트리스 격자 타일 생성, 테트로미노 큐, 테트로미노 컨트롤러
    
    1. 테트리스 격자 타일 생성
    정해진 Tile Prefab을 이용하여 width와 height 값에 맞게
    Grid를 생성함
    
    2. Tetromino Queue
    현재의 테트로미노와 다음에 나올 테트로미노를 관리하는 큐
    
    3. Tetromino Controller
    - 좌우 이동, 회전 기능 구현.
    좌우 이동, 회전 기능 모두 Grid 사이즈에 제한을 받지 않음. 즉, 범위 밖으로 나갈 수 있어서 이를 수정해야함.
    회전할 때 pivot 값을 수정해야할 것 같음.
    
    *테트로미노: 4개의 블록으로 이루어진 집합

---
+++ Tetris/Assets/Scripts/BlockController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour {
+	private GameObject childBlocks;
+
+	private bool isFalling = true;
+	private float fallCycle = 1.0f;
+
+	private void Start() {
+		StartCoroutine("FallCycle");
+	}
+
+	private void Update() {
+		ProcessInput();
+	}
+
+	private void ProcessInput() {
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			MoveBlock(Vector3.left);
+		}
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			MoveBlock(Vector3.right);
+		}
+		if(Input.GetKeyDown(KeyCode.UpArrow)) {
+			RotateBlock();
+		}
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			DropBlock();
+		}
+	}
+
+	private void MoveBlock(Vector3 dir) {
+		dir.Normalize();
+
+		transform.position += dir;
+	}
+
+	private void RotateBlock() {
+		transform.Rotate(0, 0, -90);
+	}
+
+	private void DropBlock() {
+
+	}
+
+	IEnumerator FallCycle() {
+		while (isFalling) {
+			transform.position += Vector3.down;
+
+			yield return new WaitForSeconds(fallCycle);
+		}
+	}
+}
---
+++ Tetris/Assets/Scripts/BlockManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour {
+	private bool[,] grid;
+	private GameObject[,] backgroundTiles;
+	private Queue<GameObject> tetrominoQueue = new Queue<GameObject>();
+
+	private GameObject currentTetromino = null;
+
+	[SerializeField] private GameObject bgTilePrefab;
+	[SerializeField] private GameObject[] tetrominoPrefabs;  
+	[SerializeField] private int Width = 10;
+	[SerializeField] private int Height = 20;
+
+	private void Start() {
+		grid = new bool[Width, Height];
+		backgroundTiles = new GameObject[Width, Height];
+
+		CreateBackground();
+	}
+
+	private void Update() {
+		if(tetrominoQueue.Count < 4) {
+			CreateTetromino();
+		}
+
+		if(!currentTetromino) {
+			currentTetromino = tetrominoQueue.Dequeue();
+			BlockController controller = currentTetromino.GetComponent<BlockController>();
+			if (!controller) {
+				Destroy(currentTetromino);
+				return;
+			}
+
+			currentTetromino.transform.position = new Vector3(0, Height / 2, 0);
+			controller.enabled = true;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) Destroy(currentTetromino);
+	}
+
+	private void CreateBackground() {
+		for(int i = 0; i < Width; ++i) {
+			for(int j = 0; j < Height; ++j) {
+				var tile = Instantiate(bgTilePrefab, this.transform);
+				tile.transform.position = new Vector2(i - Width / 2, j - Height / 2);
+				backgroundTiles[i, j] = tile;
+				
+			}
+		}
+	}
+
+	private void CreateTetromino() {
+		int prefabSize = tetrominoPrefabs.Length;
+		int randIndex = Random.Range(0, prefabSize);
+
+		GameObject newTetromino = Instantiate(tetrominoPrefabs[randIndex]);
+		var controller = newTetromino.GetComponent<BlockController>();
+		if (!controller) { 
+			controller = newTetromino.AddComponent<BlockController>(); 
+		}
+		controller.enabled = false;
+
+		newTetromino.transform.position = new Vector3(5 + Width / 2, 0 - 3 * tetrominoQueue.Count);
+		tetrominoQueue.Enqueue(newTetromino);
+	}
+
+	private GameObject PullTetrominoQueue() {
+		for(int i = 0; i < tetrominoQueue.Count; ++i) {
+			
+			
+		}
+	}
+}
